88da85fe0b7fc3003b5519f28715fe5fc9aedf0b
maivanphat <maiphatns98@gmail.com>
2018-03-18 10:19:59 +0700

    bt1

---
+++ bt1c2.php
@@ -0,0 +1,13 @@
+<?php
+echo "cau 1a <br><br><br>";
+$a="Chao mung cac ban den voi mon hoc lap trinh web 1";
+echo substr($a,34,15)."<br>";
+echo "-------------------------------------<br><br><br>";
+echo "cau 1b<br><br><br>";
+echo substr($a,0,4)."<br>";
+echo substr($a,5,4)."<br>";
+echo substr($a,10,3)."<br>";
+echo substr($a,14,3)."<br>";
+echo substr($a,18,3)."<br>";
+echo substr($a,21,4)."<br>";
+?>  
